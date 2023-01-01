8bcbd4880317a22f99d8a793402bb03a9ad9fea3
Sami Kuikka <sami.kuikka@aalto.fi>
2023-01-01 13:43:05 +0200

    start of the contact page

---
+++ src/components/Contact/ContactForm/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Glass from '../../Glass';
+
+const ContactForm = () => {
+
+    return (
+        <div className='h-full w-full px-[3%] py-[5%] '>
+            <Glass>
+                <div className='h-full w-full '>
+                    <div className='h-full w-full bg-blue-400 '/>
+                </div>
+            </Glass>
+        </div>
+
+        
+    );
+};
+
+export default ContactForm;
