fedd25750d21f712a176b44fda53698ae9c1b278
Sami Kuikka <sami.kuikka@aalto.fi>
2023-01-02 14:14:24 +0200

    input

--- src/components/Contact/ContactForm/Form/index.tsx
+++ src/components/Contact/ContactForm/Form/index.tsx
@@ -1,12 +1,65 @@
 import Input from "../../../Input";
 import { useState } from "react";
+import { z } from "zod";
+
+/**
+* Contact form
+* 
+*/
+
+// Validation schemas
+const nameSchema = z.string({
+    required_error: "Name is required",
+    invalid_type_error: "Name must be a string"
+}).trim().min(1, { message: 'Name can not be empty'});
+
+const emailSchema = z.string({
+    required_error: "Name is required",
+    invalid_type_error: "Name must be a string"
+}).email({message: "Must be valid email"});
+
+
+// Function for validating
+const validateInput = (validated: z.SafeParseReturnType<string, string>): string => {
+    if(!validated.success) {
+        const formattedErrors = validated.error.format();
+        return formattedErrors._errors.join(', ');
+    } else {
+        return '';
+    }
+}
 
 const Form = () => {
     const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    let nameErrors = '';
+    let emailErrors = '';
+
+    // Validations
+    const validatedName = nameSchema.safeParse(name);
+    const validatedEmail = emailSchema.safeParse(email);
+    nameErrors = validateInput(validatedName);
+    emailErrors = validateInput(validatedEmail);
     
     return (
         <div className=" px-[10%] h-full">
-            <Input type="text" value={name} onInput={({target}) => setName((target as HTMLInputElement).value)}/>
+            <Input 
+                type="text" 
+                value={name} 
+                onInput={({target}) => setName((target as HTMLInputElement).value)}
+                label="Name"
+                textHelper="Your name"
+                errors={nameErrors}
+            />
+            <Input
+                type="email"
+                value={email}
+                onInput={({target}) => setEmail((target as HTMLInputElement).value)}
+                label="Email"
+                textHelper="Your email where I can contact you"
+                errors={emailErrors}
+            />
         </div>
     );
 };
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,6 +13,7 @@ interface InputProps {
     label?: string;
     textHelper?: string;
     required?: true;
+    errors?: string;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -21,12 +22,17 @@ const Input: React.FC<InputProps> = ({
     onInput,
     label,
     textHelper,
-    required
+    required,
+    errors
 }) => {
 
+    const [ touched, setTouched] = useState(false);
+
+      // Show error when input contains error and user has touched the input
+      const showError: boolean = touched && errors != undefined && errors.length > 0;
 
     return (
-        <label className="relative">
+        <label className="relative block">
             <input
                 type={type}
                 spellCheck="false"
@@ -36,10 +42,36 @@ const Input: React.FC<InputProps> = ({
                     "min-w-[280px] w-full h-12 pl-4 pr-3 rounded-t-lg peer",
                     "font-sans text-base font-normal transition-colors",
                     "bg-gradient-to-tl from-violet-400/80 via-rose-500/60 to-fuchsia-400 ",
-                    "border-b border-black focus:outline-none focus:border-b-2"
+                    "border-b border-black focus:outline-none focus:border-b-2",
+                    "placeholder:text-transparent placeholder:select-none cursor-text",
+                    { "border-red-500": showError}
+                )}
+                placeholder="placeholder"
+                required={required}
+                onChange={() => setTouched(true)}
+            />
+            {/** Placeholder */}
+            <span
+                className={classNames(
+                    // Positions
+                    'absolute select-none left-0 top-0 peer-placeholder-shown:top-3.5',
+                    // Text
+                    'text-primary text-xs peer-placeholder-shown:text-gray-800 peer-placeholder-shown:text-base ',
+                    // other
+                    'pl-4 transition-all cursor-text'
                 )}
             >
-            </input>
+                {label}
+            </span>
+
+            {/** Helpers at the bottom */}
+            <div className={classNames(" pt-1 px-4 text-xs min-w-[280px] text-neutral-30 ",
+                    {"text-red-500": showError}
+                )}
+            >
+                    {showError ? errors : textHelper}
+            </div>
+            
         </label>
     );
 }
