676379c1aa175c5de38449c155a909bc9f0faac1
Sami Kuikka <sami.kuikka@aalto.fi>
2023-01-06 15:21:03 +0200

    all pages, navigation + resume

--- src/components/Background/StudyRecords/Schools/index.tsx
+++ src/components/Background/StudyRecords/Schools/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import School from './School';
 
 const data = [
-    {
+   /*  {
         info: "Arkadia Yhteislyseo (2015-2018)",
         description: "I did my upper secondary school at Arkadia Yhteislyseo at Nurmijärvi."
-    },
+    }, */
     {
         info: "Aalto University - Computer Science (Bachelor’s level) (2019- )",
         description: "I started my Bachelor’s degree at 2019. 175 / 180 ECTS credits done (1 course away from graduation) 4,58 GPA"
--- src/components/Introduction/Actions/index.tsx
+++ src/components/Introduction/Actions/index.tsx
@@ -2,6 +2,20 @@ import PrimaryButton from '../../buttons/PrimaryButton';
 
 const Actions = () => {
 
+    const showResume= () => {
+        fetch('Resume.pdf').then(response => {
+            response.blob().then(blob => {
+
+                const fileURL = window.URL.createObjectURL(blob);
+
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Resume.pdf';
+                alink.click();
+            })
+        })
+    }
+
     return (
         <div
             className='flex flex-col mobile:flex-row mobile:h-14 mobile:pl-5 z-[3]'
@@ -9,11 +23,11 @@ const Actions = () => {
             <PrimaryButton 
                 name="My Resume"
                 icon="DownloadIcon"
-                onClick={() => alert('Sorry, under development')}
+                onClick={showResume}
             />
             <PrimaryButton 
                 name="Contact"
-                onClick={() => console.log('hello')}
+                onClick={() => location.href="#contact"}
             />
         </div>
     );
