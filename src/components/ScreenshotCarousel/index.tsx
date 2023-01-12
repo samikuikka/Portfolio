dfa19f6e88730594650c4a510a6586ad1460243a
Sami Kuikka <sami.kuikka@aalto.fi>
2023-01-12 17:13:09 +0200

    gap between screenshots in carousel

--- src/components/ScreenshotCarousel/index.tsx
+++ src/components/ScreenshotCarousel/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { CustomArrowNext, CustomArrowPrev } from '../Projects/Carousel/CustomArrow';
+import './screenshots.css';
+
 
 const settings = {
     dots: true,
@@ -21,15 +22,23 @@ const settings = {
     ]
 }
 
-const ScreenshotCarousel = () => {
+interface CarouselProps {
+    screenshots: [string];
+}
+
+const ScreenshotCarousel: React.FC<CarouselProps> = ({
+    screenshots
+}) => {
 
     return (
         <div className='block not-prose pb-10 w-full'>
             <div className='max-w-[80vw] md:max-w-none m-auto'>
                 <Slider {...settings}>
-                    <img src="https://placekitten.com/1920/1080" />
-                    <img src="https://placekitten.com/800/450" />
-                    <img src="https://placekitten.com/1000/500" />
+                    {screenshots.map((src, i) => {
+                        return (
+                            <img src={src} />
+                        )
+                    })}
                 </Slider>
             </div>
         </div>
